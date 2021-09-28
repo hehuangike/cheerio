26fe300e6ab8f6df621b493a2a840240bc2bb660
he.huang <he.huang@ike-data.com>
2021-09-28 15:01:16 +0800

    调整页面样式

--- IKEdata/views.py
+++ IKEdata/views.py
@@ -37,7 +37,7 @@ def loginto(request):
         else:
             tips = '用户不存在'
     else:
-        tips = '您需要填写用户名和密码才能登录'
+        tips = '请输入正确的账号和密码'
     return render(request, 'base.html', {'tips': tips, 'una': una})
 
 
@@ -78,14 +78,16 @@ def dashboard(request):
     other_data = round(other_no / total_no * 100)
     positive_no = jd_no * 0.99
     negative_no = jd_no * 0.01
+    current = 'dashboard'
     #     Input one page for text analysis
-    return render(request, 'Container/Dashboard/dashboard.html', locals())
+    return render(request, 'Container/Dashboard/index.html', locals())
 
 
 def analysis_test(request):
     context = request.POST.get('context', None)
     extract_text = ''
     analysis_result = False
+    current = 'analysis_test'
     if context:
         extract_text, keywords = extract2.extract(context)
         models.collect.objects.create(
@@ -96,12 +98,14 @@ def analysis_test(request):
             link='',
         )
         analysis_result = True
+        current = 'analysis_result'
     return render(
         request, 'Container/Analysis/analysisTest.html',
         {
             "context": context,
             "extract_text": extract_text,
-            'analysis_result': analysis_result
+            'analysis_result': analysis_result,
+            'current': current,
         }
     )
 
@@ -122,12 +126,12 @@ def collect(request):
     #     page_obj = paginator.page(1)
     # except PageNotAnInteger as not_int_err:
     #     page_obj = paginator.page(1)
-    return render(request, 'Container/Collect/collect.html', {"all_data": all_data})
+    return render(request, 'Container/Collect/index.html', {"all_data": all_data, "current": "collect"})
 
 
 def clean(request):
     #   DataClean data
-    return render(request, 'Container/Clean/clean.html', locals())
+    return render(request, 'Container/Clean/index.html', {"current": "clean"})
 
 
 def analysis(request):
@@ -141,12 +145,14 @@ def analysis(request):
     #         exit()
     # extract_text = analyse_text.extract(comments)
     #   Display key words, word cloud, word net, text abstract
-    return render(request, 'Container/Analysis/analysis.html', locals())
+    
+    context = "用了以后再追评一直用这个机油给车子保养，非常不错，相信京东自营。不错，美孚一号大牌子，京东自营值得信赖，爱车非常适用，已经用好多次京东买了，顺便购买了更换机油服务，一步到位，发动机很安静，油耗也降低了一点点，谁用谁知道，希望多搞点活动。一直用的银美孚一号 价格合适 自己保养车更放心 物流很快 一直信赖京东自营的东西选机油，先选SN级的，这是最高级，SL级是低配款，0W-30的意思是机油适用零度，流动性30的。家中常备机油，便宜就先囤一个，一直用这个牌子推荐这家店铺非常不错哦第一次在京东平台上购买美孚一号，之前一直在市场上买，效果特别好，希望京东平台上的美孚保真保质，如果用的好，以后还会接着在京东上购买京东平台优惠力度大，值得推荐，大家快来抢购，以后还会在京东上购买，希望以后赠送礼品多点首先，金美孚一号这款油，一直用这个牌子，质量非常好，其次，京东自营下单，配送时效特别快，晚上下单第二天早上9点就收到了，省去了去实体店的苦恼，而且价格也非常优惠，有优惠券可以领，这款油保养周期长，相信京东自营商城，物流很快，拿去店里换的，维修师傅说有不错，换完车的噪声小了很多，还在在买的发动机是汽车的心脏，润滑油的好坏对车辆的健康与否密切相关。好评一直从京东购买这款美孚一号机油，每次一万公里保养一次。"
+    return render(request, 'Container/Analysis/analysis.html', {"context": context, "current": "analysis"})
 
 
 def mining(request):
     #   To be updated
-    return render(request, 'Container/Mining/mining.html', locals())
+    return render(request, 'Container/Mining/index.html', {"current": "mining"})
 
 
 def wordnet(request):
